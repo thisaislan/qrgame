b6721ccf161466aa20ab02a81d1ae9016f8b0fc3
thisaislan <this.aislan@gmail.com>
2021-01-25 19:35:44 -0300

    :sparkles: Create commons behaviors

--- qrgame-andorid-app/base/src/main/java/thisaislan/qrgame/base/BaseActivity.kt
+++ qrgame-andorid-app/base/src/main/java/thisaislan/qrgame/base/BaseActivity.kt
@@ -1,5 +1,33 @@
 package thisaislan.qrgame.base
 
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+
 open class BaseActivity : AppCompatActivity() {
+
+    private val exitToast: Toast by lazy(LazyThreadSafetyMode.NONE) {
+        Toast.makeText(this@BaseActivity, getString(R.string.try_exit), Toast.LENGTH_SHORT)
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        }
+    }
+
+    override fun onBackPressed() {
+        if (exitToast.view.windowToken == null)
+            exitToast.show()
+        else {
+            finish()
+            exitToast.cancel()
+        }
+    }
 }
