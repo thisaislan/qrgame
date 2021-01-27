2c5891479ce7801ca96b790462246bd71bb27e80
thisaislan <this.aislan@gmail.com>
2021-01-26 23:27:41 -0300

    :sparkles: Create splash activity

---
+++ qrgame-andorid-app/app/src/main/java/thisaislan/qrgame/SplashActivity.kt
@@ -0,0 +1,11 @@
+package thisaislan.qrgame
+
+import android.os.Bundle
+import thisaislan.qrgame.base.BaseActivity
+
+class SplashActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
