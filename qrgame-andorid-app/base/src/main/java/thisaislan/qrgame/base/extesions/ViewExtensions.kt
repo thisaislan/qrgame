af3bf35e23cb37e09b6b77f0e87fc38099f2f0c3
thisaislan <this.aislan@gmail.com>
2021-01-30 18:29:39 -0300

    :sparkles: Create ViewExtensions

---
+++ qrgame-andorid-app/base/src/main/java/thisaislan/qrgame/base/extesions/ViewExtensions.kt
@@ -0,0 +1,78 @@
+package thisaislan.qrgame.base.extesions
+
+import android.view.View
+import android.view.animation.*
+import android.view.animation.Animation.AnimationListener
+import androidx.annotation.IntegerRes
+import thisaislan.qrgame.base.R
+
+fun View.visible() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.startFadeInAnimation(
+    @IntegerRes animationDurationId: Int = R.integer.animation_medium_duration,
+    interpolator: BaseInterpolator = AccelerateDecelerateInterpolator(),
+    onAnimationEnd: (() -> Unit)? = null,
+    onAnimationRepeat: (() -> Unit)? = null,
+    onAnimationStart: (() -> Unit)? = null,
+) {
+    startAlphaAnimation(
+        0f,
+        1f,
+        animationDurationId,
+        interpolator,
+        onAnimationEnd,
+        onAnimationRepeat,
+        onAnimationStart
+    )
+}
+
+private fun View.startAlphaAnimation(
+    fromAlpha: Float,
+    toAlpha: Float,
+    @IntegerRes animationDurationId: Int,
+    interpolator: Interpolator,
+    onAnimationEnd: (() -> Unit)? = null,
+    onAnimationRepeat: (() -> Unit)? = null,
+    onAnimationStart: (() -> Unit)? = null
+) {
+    val alphaAnimation: Animation = AlphaAnimation(fromAlpha, toAlpha)
+
+    startAnimation(
+        alphaAnimation,
+        animationDurationId,
+        interpolator,
+        onAnimationEnd,
+        onAnimationRepeat,
+        onAnimationStart
+    )
+}
+
+private fun View.startAnimation(
+    animation: Animation,
+    @IntegerRes animationDurationId: Int,
+    interpolator: Interpolator,
+    onAnimationEnd: (() -> Unit)? = null,
+    onAnimationRepeat: (() -> Unit)? = null,
+    onAnimationStart: (() -> Unit)? = null
+) {
+    animation.duration = resources.getIntegerAsLong(animationDurationId)
+    animation.interpolator = interpolator
+
+    animation.setAnimationListener(object : AnimationListener {
+        override fun onAnimationEnd(animation: Animation) {
+            onAnimationEnd?.invoke()
+        }
+
+        override fun onAnimationRepeat(animation: Animation) {
+            onAnimationRepeat?.invoke()
+        }
+
+        override fun onAnimationStart(animation: Animation) {
+            onAnimationStart?.invoke()
+        }
+    })
+
+    this.startAnimation(animation)
+}
