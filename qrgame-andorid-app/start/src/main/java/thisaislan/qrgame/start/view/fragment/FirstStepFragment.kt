3c70c676986b0b9c0968dc3e73243825391303da
thisaislan <this.aislan@gmail.com>
2021-05-11 18:51:38 -0300

    :sparkles: Create navigation flow of module

---
+++ qrgame-andorid-app/start/src/main/java/thisaislan/qrgame/start/view/fragment/FirstStepFragment.kt
@@ -0,0 +1,51 @@
+package thisaislan.qrgame.start.view.fragment
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import thisaislan.qrgame.base.extesions.cameraPermissionDenied
+import thisaislan.qrgame.base.extesions.sharedPreferences
+import thisaislan.qrgame.start.R
+import thisaislan.qrgame.start.databinding.FragmentFirstStepBinding
+
+class FirstStepFragment : Fragment(R.layout.fragment_first_step) {
+
+    companion object {
+        const val CAMERA_REQUEST = 15348
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        FragmentFirstStepBinding.bind(view).initializeViews()
+    }
+
+    private fun FragmentFirstStepBinding.initializeViews() =
+        confirmationButton.setOnClickListener {
+            requestPermissions(arrayOf(Manifest.permission.CAMERA), CAMERA_REQUEST)
+        }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == CAMERA_REQUEST) {
+
+            if (grantResults.isNotEmpty()) {
+                var destination = R.id.action_firstStepFragment_to_welcomeFragment
+
+                if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                    destination = R.id.action_firstStepFragment_to_needPermissionFragment
+                    requireContext().sharedPreferences.cameraPermissionDenied()
+                }
+                Navigation.findNavController(requireView()).navigate(destination)
+            }
+        }
+    }
+
+}
---
+++ qrgame-andorid-app/start/src/main/java/thisaislan/qrgame/start/view/fragment/ReadQrCodeFragment.kt
@@ -0,0 +1,77 @@
+package thisaislan.qrgame.start.view.fragment
+
+import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.budiyev.android.codescanner.ScanMode
+import com.google.zxing.Result
+import thisaislan.qrgame.base.view.dialog.NeutralDialog
+import thisaislan.qrgame.start.R
+import thisaislan.qrgame.start.databinding.FragmentReadQrCodeBinding
+
+class ReadQrCodeFragment : Fragment(R.layout.fragment_read_qr_code) {
+
+    private lateinit var codeScanner: CodeScanner
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        FragmentReadQrCodeBinding.bind(view).initializeViews()
+        scanSettings()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
+    }
+
+    private fun FragmentReadQrCodeBinding.initializeViews() {
+        codeScanner = CodeScanner(requireContext(), scannerView)
+        back.setOnClickListener { navigateBack() }
+    }
+
+    private fun scanSettings() =
+        with(codeScanner) {
+            camera = CodeScanner.CAMERA_BACK
+            formats = CodeScanner.ALL_FORMATS
+
+            autoFocusMode = AutoFocusMode.SAFE
+            scanMode = ScanMode.SINGLE
+            isAutoFocusEnabled = true
+            isFlashEnabled = false
+
+            decodeCallback = DecodeCallback { runOnMainThread { successfulReading(it) } }
+            errorCallback = ErrorCallback { runOnMainThread { errorWhileReading() } }
+        }
+
+    private fun runOnMainThread(unitToRun: () -> Unit) =
+        ContextCompat.getMainExecutor(requireContext()).execute { unitToRun.invoke() }
+
+    private fun successfulReading(result: Result) {
+        val action = ReadQrCodeFragmentDirections
+            .actionReadQRCodeFragmentToProcessingFragment(result.text)
+
+        Navigation.findNavController(requireView()).navigate(action)
+    }
+
+    private fun errorWhileReading() =
+        NeutralDialog(
+            context = requireContext(),
+            message = R.string.while_reading_qr_code
+        ) { navigateBack() }.show()
+
+    private fun navigateBack() =
+        Navigation.findNavController(requireView())
+            .navigate(R.id.action_readQRCodeFragment_to_welcomeFragment)
+
+}
