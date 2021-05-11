3fee954f83a698fad7ea1a8c32ed3f91381ed503
thisaislan <this.aislan@gmail.com>
2021-05-11 19:04:05 -0300

    :recycle: Refactor SplashActivity

--- qrgame-andorid-app/app/src/main/java/thisaislan/qrgame/SplashActivity.kt
+++ qrgame-andorid-app/app/src/main/java/thisaislan/qrgame/SplashActivity.kt
@@ -1,27 +1,22 @@
 package thisaislan.qrgame
 
-import android.Manifest
-import android.content.pm.PackageManager
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import androidx.core.content.ContextCompat
 import thisaislan.qrgame.base.BaseActivity
 import thisaislan.qrgame.base.extesions.getIntegerAsLong
 import thisaislan.qrgame.base.extesions.startFadeInAnimation
 import thisaislan.qrgame.base.extesions.visible
 import thisaislan.qrgame.base.singletons.AudioPlayer
 import thisaislan.qrgame.databinding.ActivitySplashBinding
+import thisaislan.qrgame.start.view.activity.StartActivity
 
 class SplashActivity : BaseActivity() {
-
-    private val biding: ActivitySplashBinding by lazy(LazyThreadSafetyMode.NONE) {
-        ActivitySplashBinding.inflate(layoutInflater)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        with(biding) {
+        with(ActivitySplashBinding.inflate(layoutInflater)) {
             setContentView(root)
 
             logo.startFadeInAnimation(
@@ -33,19 +28,9 @@ class SplashActivity : BaseActivity() {
                     AudioPlayer.playAudio(this@SplashActivity, R.raw.ding)
 
                     Handler().postDelayed({
-                        checkPermissions()
                     }, resources.getIntegerAsLong(R.integer.animation_medium_duration))
                 })
         }
     }
 
-    private fun checkPermissions() {
-        if (ContextCompat.checkSelfPermission(this@SplashActivity, Manifest.permission.CAMERA)
-            != PackageManager.PERMISSION_GRANTED
-        ) {
-            // Call scene to ask permissions
-        } else {
-            // Call scene to read
-        }
-    }
 }
