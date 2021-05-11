2e4fec763a58354c2113e73e73457e47e75b1197
thisaislan <this.aislan@gmail.com>
2021-05-11 18:28:58 -0300

    :sparkles: Create Constants file on base module

---
+++ qrgame-andorid-app/base/src/main/java/thisaislan/qrgame/base/util/Constants.kt
@@ -0,0 +1,10 @@
+package thisaislan.qrgame.base.util
+
+class Constants {
+
+    object FileNames {
+        const val QR_GAME_SCRIPT_FOLDER = "core"
+        const val QR_GAME_SCRIPT = "core.js"
+    }
+
+}
