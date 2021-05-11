9a26c1743db9d0d370624b9cb86cb3e08bd29e8e
thisaislan <this.aislan@gmail.com>
2021-05-11 18:33:58 -0300

    :sparkles: Create SharedPreferencesExtension

---
+++ qrgame-andorid-app/base/src/main/java/thisaislan/qrgame/base/extesions/SharedPreferencesExtension.kt
@@ -0,0 +1,15 @@
+package thisaislan.qrgame.base.extesions
+
+import android.Manifest
+import android.content.SharedPreferences
+
+fun SharedPreferences.hasPreviousDenialCameraPermission(): Boolean =
+    getInt(Manifest.permission.CAMERA, 0) != 0
+
+fun SharedPreferences.cameraPermissionDenied() {
+    edit().putInt(Manifest.permission.CAMERA, 1).apply()
+}
+
+fun SharedPreferences.clear() {
+    edit().clear().apply()
+}
