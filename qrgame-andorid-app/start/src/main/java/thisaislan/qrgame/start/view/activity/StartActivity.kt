c0c7ebb66f5210e0796fd4a6b00c09465dd473cf
thisaislan <this.aislan@gmail.com>
2021-05-11 18:52:06 -0300

    :sparkles: Create StarActivity

---
+++ qrgame-andorid-app/start/src/main/java/thisaislan/qrgame/start/view/activity/StartActivity.kt
@@ -0,0 +1,38 @@
+package thisaislan.qrgame.start.view.activity
+
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import thisaislan.qrgame.base.BaseActivity
+import thisaislan.qrgame.base.extesions.sharedPreferences
+import thisaislan.qrgame.base.extesions.hasPreviousDenialCameraPermission
+import thisaislan.qrgame.base.extesions.isCameraPermissionGranted
+import thisaislan.qrgame.start.R
+
+class StartActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+
+        navigateTo(getFirstView())
+    }
+
+    private fun getFirstView() =
+        if (isCameraPermissionGranted()) {
+            R.id.welcomeFragment
+        } else {
+            if (sharedPreferences.hasPreviousDenialCameraPermission()) {
+                R.id.needPermissionFragment
+            } else {
+                R.id.firstStepFragment
+            }
+        }
+
+    private fun navigateTo(destination: Int) =
+        (supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment).run {
+            val navGraph = navController.navInflater.inflate(R.navigation.nav_start)
+            navGraph.startDestination = destination
+            navController.graph = navGraph
+        }
+
+}
